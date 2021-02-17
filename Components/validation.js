9e8431ded9ad75bf76f2631f7374b1898340887e
Komplett-Jamie <78380034+Komplett-Jamie@users.noreply.github.com>
2021-02-17 13:00:32 +0100

    update

---
+++ Components/validation.js
@@ -0,0 +1,16 @@
+function validatePassword(){
+    
+    let password = document.getElementById("password");
+    let confirm_password = document.getElementById("confirm_password");
+
+    if(password.value.length < 8) {
+        confirm_password.setCustomValidity("Passowrd must be 8 characters or more!")
+    }
+     else if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
