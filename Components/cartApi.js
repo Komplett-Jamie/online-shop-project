28b97b833447d3b035985ee07b1577914dbd826f
Komplett-Jamie <78380034+Komplett-Jamie@users.noreply.github.com>
2021-03-01 22:20:23 +0100

    updatye

--- Components/cartApi.js
+++ Components/cartApi.js
@@ -44,29 +44,72 @@ async function checkCartItems() {
 countCartItems(cartNumber.items);
 }
 
-function countCartItems(value)   {
+async function countCartItems(value)   {
     let numberOfCartItems = [];
-    let object = value;
+    let objectCart = value;
 
-    for (var i = 0; i < object.length; i++)    {
-        numberOfCartItems.push(object[i].quantity);
+    for (var i = 0; i < objectCart.length; i++)    {
+        numberOfCartItems.push(objectCart[i].quantity);
     }
     let totalCartItems = numberOfCartItems.reduce((total, n) => total + n, 0);
     document.getElementById("small_cart").innerHTML = `<span>${totalCartItems}</span>`;
-    return renderCartBigCart(object);
+    renderCartBigCart(objectCart);
 }
 
-function renderCartBigCart(object)    {
-    console.log(object);
-  
+function renderCartBigCart(objectCart)    {
+console.log(objectCart);
+let placing = document.getElementById("cart-items");
 
-//MAKE THIS TO SHOW PRODUCTS AS LIST ON CART PAGE.
-    for (var i = 0; i < object.length; i++) {
-        document.getElementById("cartList").innerHTML += object[i].productId;
+    for (var i = 0; i < objectCart.length; i++) {
+        console.log(objectCart[i].productName)
+        placing.innerHTML += `
+        <div class="cart-item">
+            <div class="cart-item-image">
+                <div class="item-image">
+                    <img src="https://komplett.no/img/p/200/1155451.jpg">
+                </div>
+            </div>
+            <div class="cart-item-name">${objectCart[i].productName}</div>
+            <div class="cart-item-change-amount">
+                <a class="cart-item-add-reduce">+</a>
+                <input type="tel" placeholder="${objectCart[i].quantity}">
+                <a class="cart-item-add-reduce">-</a>
+            </div>
+            <div class="cart-item-total">
+                <span class="cart-item-total-for-product">${objectCart[i].price}</span>
+            <a class="cart-delete-item">X</a>
+            </div>
+        </div>
+        `;
     }
 
 }
 
+
+
+
+//------------------------------------
+
+function renderRandomProducts(randomProductObject) {
+    let object = randomProductObject;
+    let placing = document.getElementById("random-products");
+
+    for (var i = 0; i < object.length; i++) {
+        placing.innerHTML += 
+        `<a href="./../pages/productPage.html?id=${object[i].id}">
+            <div class="product">
+                <div class="product-name">
+                    <p>${object[i].name}</p>
+                </div>
+                <br>
+                <img alt="${object[i].description}" src="${object[i].imageUrl}">
+                <p>Nå: <b>${object[i].price},-</b></p>
+                </a>
+                <button class="add-to-cart-button" id="addToCart" onclick="addProductToCart()">Add to Cart</button>
+            </div>
+        `
+    }
+}
 
 
 
