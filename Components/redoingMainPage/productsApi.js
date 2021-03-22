bd0c013ff7f46cb535a1472bbb673887e3355d9b
Komplett-Jamie <78380034+Komplett-Jamie@users.noreply.github.com>
2021-03-22 22:37:28 +0100

    update

--- Components/redoingMainPage/productsApi.js
+++ Components/redoingMainPage/productsApi.js
@@ -1,15 +1,3 @@
-subscribeToEvent("pageLoad", async function frontPageRandomProducts()  {
-    const CategoriesUrl = "https://jamiestore.herokuapp.com/Products/Random/10";
-
-    const curlDetails = {
-        method: 'GET',
-    };
-
-    let apiFetch = await fetch(CategoriesUrl, curlDetails);
-    let response = await apiFetch.json();
-    publishEvent("randomProducts", response);
-})
-
 subscribeToEvent("pageLoad", async function fetchProduct() {
 
     const urlParams = new URLSearchParams(location.search);
@@ -24,5 +12,4 @@ subscribeToEvent("pageLoad", async function fetchProduct() {
 let apiFetch = await fetch(userApi, UserDetails)
 let response = await apiFetch.json();
 publishEvent("fetchedProduct", response)
-// return renderProduct(response);
 })
