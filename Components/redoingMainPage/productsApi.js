1353cbc5a8673dd63585bec9b93e714057cbb6cb
Komplett-Jamie <78380034+Komplett-Jamie@users.noreply.github.com>
2021-04-14 08:54:06 +0200

    cleaning up code and removing dead links

--- Components/redoingMainPage/productsApi.js
+++ Components/redoingMainPage/productsApi.js
@@ -1,8 +1,6 @@
 subscribeToEvent("pageLoad", async function fetchProduct() {
-
     const urlParams = new URLSearchParams(location.search);
     let productId = urlParams.get("id");
-
     let productApiCall = new ProductsApi();
     let response = await productApiCall.productById(productId);
 publishEvent("fetchedProduct", await response.json())
--- Components/redoingMainPage/renderProduct.js
+++ Components/redoingMainPage/renderProduct.js
@@ -1,5 +1,4 @@
 subscribeToEvent("fetchedProduct", function(response)    {
-
     document.getElementById("product-Id").innerHTML = response.id;
     document.getElementById("product-name").innerHTML = response.name;
     document.getElementById("product-description").innerHTML = response.description;
