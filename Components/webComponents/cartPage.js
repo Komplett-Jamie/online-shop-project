063cfe0b5ecba78d2dcc018978682d206a991ae3
Kristoffer Ravik Andresen <kristoffer.ravikandresen@komplett.com>
2021-05-27 10:08:14 +0200

    Cart items are fetched from the API when CartPage loads

--- Components/webComponents/cartPage.js
+++ Components/webComponents/cartPage.js
@@ -1,9 +1,16 @@
-﻿export class CartPage extends HTMLElement {
+﻿import { CartApi } from "../../API/CartApi.js";
+
+export class CartPage extends HTMLElement {
   constructor() {
     super();
+    this.items = [];
   }
 
   async connectedCallback() {
+    let cartApi = new CartApi();
+    let cart = await cartApi.fetchCart();
+    this.items = cart.items;
+    
     this.innerHTML = `
         <big-cart></big-cart>
         <cart-forms></cart-forms>
